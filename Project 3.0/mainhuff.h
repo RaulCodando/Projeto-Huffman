13ba710c61063105ec0bc5db75d42fce8f3e0842
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-03-17 12:05:41 -0300

    Update mainhuff.h

--- Project 3.0/mainhuff.h
+++ Project 3.0/mainhuff.h
@@ -1,1 +1,12 @@
+#ifndef MAINHUFF_H
+#define MAINHUFF_H
 
+#include "queue.h"
+#include "mapbytes.h"
+#include <string.h>
+
+long int fileSize(FILE *f);
+int compressFile();
+int decompressFile();
+
+#endif //MAINHUFF_H
