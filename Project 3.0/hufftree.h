6f3f8193d13490ec5b86244c3482e537361b27b9
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-03-17 12:13:54 -0300

    Update hufftree.h

--- Project 3.0/hufftree.h
+++ Project 3.0/hufftree.h
@@ -1,1 +1,14 @@
+#ifndef HUFFTREE_H
+#define HUFFTREE_H
 
+#include "queue.h"
+
+int isEmptyList(Node *head);
+int max(int a, int b);
+Node *createBinaryHuffmanTree(Node *huffmanList);
+int height(Node *huffmanTree);
+void saveHuffmanTreePreOrder(Node *huffmanTree, unsigned char array[], int *i, int *treeSize);
+void printHuffmanTreePreOrder(Node *huffmanTree);
+Node *rebuildTree(unsigned char string[], int size, int *i, Node *tree);
+
+#endif //HUFFTREE_H
