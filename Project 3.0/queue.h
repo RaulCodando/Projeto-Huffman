4eed0e29b83ff95a16cdcaec54c3d8f799482524
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-03-17 12:10:47 -0300

    Update queue.h

--- Project 3.0/queue.h
+++ Project 3.0/queue.h
@@ -1,1 +1,24 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    void *item;
+    struct node *next;
+    void * frequency;
+    struct node *left;
+    struct node *right;
+}Node;
+
+typedef struct priorityQueue{
+    Node *head;
+}Queue;
+
+Queue *createHuffmanQueue();
+int isEmpty(Queue *fila);
+void enqueue(Queue *fila, unsigned char item, int frequency);
+void printQueue(Queue *fila);
+
+#endif //QUEUE_H
